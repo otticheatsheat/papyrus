b0c67d8710136cd4365d28e0948cc7cd0fe3b28b
Shahak Shama <shahak@starkware.co>
2023-10-08 17:37:04 +0300

    WIP (session compiles, handler needs to be modified to support new session)

--- crates/papyrus_network/src/streamed_data_protocol/handler/session.rs
+++ crates/papyrus_network/src/streamed_data_protocol/handler/session.rs
@@ -5,7 +5,7 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 use futures::future::BoxFuture;
-use futures::{AsyncWriteExt, FutureExt};
+use futures::{select, AsyncReadExt, AsyncWriteExt, FutureExt};
 use libp2p::swarm::Stream;
 use replace_with::replace_with_or_abort;
 
@@ -15,16 +15,28 @@ use crate::messages::write_message;
 pub(super) struct InboundSession<Data: DataBound> {
     pending_messages: VecDeque<Data>,
     current_task: WriteMessageTask,
+    buffer: [u8; 1],
+}
+
+pub(super) enum InboundSessionError {
+    IO(io::Error),
+    OtherPeerSentData,
 }
 
 pub(super) enum FinishReason {
-    Error(io::Error),
+    Error(InboundSessionError),
     Closed,
+    OtherPeerClosed,
+}
+
+enum RunningFinishReason {
+    Error(InboundSessionError),
+    OtherPeerClosed,
 }
 
 enum WriteMessageTask {
     Waiting(Stream),
-    Running(BoxFuture<'static, Result<Stream, io::Error>>),
+    Running(BoxFuture<'static, Result<Stream, RunningFinishReason>>),
     Closing(BoxFuture<'static, Result<(), io::Error>>),
 }
 
@@ -35,6 +47,7 @@ impl<Data: DataBound> InboundSession<Data> {
         Self {
             pending_messages: Default::default(),
             current_task: WriteMessageTask::Waiting(stream),
+            buffer: Default::default(),
         }
     }
 
@@ -68,7 +81,29 @@ impl<Data: DataBound> InboundSession<Data> {
                 };
                 WriteMessageTask::Running(
                     async move {
-                        write_message(data, &mut stream).await?;
+                        select! {
+                            result = write_message(data, &mut stream)
+                                .map(|result| result.map_err(|io_error| {
+                                    RunningFinishReason::Error(InboundSessionError::IO(io_error))
+                                })).fuse() => result,
+                            size_result = stream.read(&mut self.buffer).fuse() => {
+                                match size_result {
+                                    Err(io_error) => {
+                                        Err(RunningFinishReason::Error(
+                                            InboundSessionError::IO(io_error)
+                                        ))
+                                    },
+                                    Ok(0) => {
+                                        Ok(())
+                                    },
+                                    _ => {
+                                        Err(RunningFinishReason::Error(
+                                            InboundSessionError::OtherPeerSentData
+                                        ))
+                                    }
+                                }
+                            }
+                        }?;
                         Ok(stream)
                     }
                     .boxed(),
@@ -89,7 +124,10 @@ impl<Data: DataBound> InboundSession<Data> {
                 self.current_task = WriteMessageTask::Waiting(stream);
                 self.handle_waiting(cx)
             }
-            Poll::Ready(Err(io_error)) => Some(FinishReason::Error(io_error)),
+            Poll::Ready(Err(RunningFinishReason::Error(error))) => Some(FinishReason::Error(error)),
+            Poll::Ready(Err(RunningFinishReason::OtherPeerClosed)) => {
+                Some(FinishReason::OtherPeerClosed)
+            }
         }
     }
 
@@ -100,7 +138,9 @@ impl<Data: DataBound> InboundSession<Data> {
         match fut.poll_unpin(cx) {
             Poll::Pending => None,
             Poll::Ready(Ok(())) => Some(FinishReason::Closed),
-            Poll::Ready(Err(io_error)) => Some(FinishReason::Error(io_error)),
+            Poll::Ready(Err(io_error)) => {
+                Some(FinishReason::Error(InboundSessionError::IO(io_error)))
+            }
         }
     }
 }
